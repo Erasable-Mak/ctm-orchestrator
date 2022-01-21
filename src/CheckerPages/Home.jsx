3bf9aebff3c1c9529626b042c6e7d4e6455d556e
Hrushikesh Sanjay Malshikare <hrushikeshmalshikare710@gmail.com>
2022-01-21 13:06:20 +0530

    LandSiteBuilding updated

--- src/CheckerPages/Home.jsx
+++ src/CheckerPages/Home.jsx
@@ -1,108 +1,179 @@
 import React, { useState } from "react";
-import DefaultPage from "./DefaultPage";
-
-import
-{
-    AppBar,
-    Box,
-    CssBaseline,
-    Divider,
-    Drawer,
-    List,
-    ListItem,
-    ListItemText,
-    Toolbar,
-    Typography,
-    Button,
-} from "@mui/material";
-import LogoutIcon from "@mui/icons-material/Logout";
 
 import { useAuth } from "../contexts/AuthContext";
+import clsx from "clsx";
+import { Menu, Close } from "@material-ui/icons";
+
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+} from "@mui/material";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { makeStyles } from "@material-ui/core";
+
+import DefaultPage from "./DefaultPage";
 
 const drawerWidth = 240;
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  appBar: {
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  hide: {
+    display: "none",
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: "flex",
+    alignItems: "center",
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: "flex-end",
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: 0,
+  },
+  contentShift: {
+    transition: theme.transitions.create("margin", {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: drawerWidth,
+  },
+}));
 
-export default function Home()
-{
-    const [contentName, setcontentName] = useState("DefaultPage");
+export default function Home() {
+  const [contentName, setcontentName] = useState("Create User");
 
-    const { currentUser, logout } = useAuth();
+  const { currentUser, logout } = useAuth();
+  const [open, setOpen] = useState(false);
+  const classes = useStyles();
 
-    const changeContent = (value) =>
-    {
-        setcontentName(value);
-    };
+  const changeContent = (value) => {
+    setcontentName(value);
+  };
 
-    return (
-        <Box sx={{ display: "flex" }}>
-            <CssBaseline />
-            <AppBar
-                position="fixed"
-                sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+  const handleDrawer = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar
+          position="fixed"
+          sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        >
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawer}
+              edge="start"
+              className={clsx(classes.menuButton)}
             >
-                <Toolbar>
-                    <Typography variant="h6" noWrap component="div">
-                        Makarand Rajendra
-                    </Typography>
-                    <Box sx={{ flexGrow: 1 }} />
-                    <Box sx={{ display: { xs: "flex" } }}>
-                        <Typography variant="h6" noWrap component="div">
-                            {currentUser?.email}
-                        </Typography>
-                        <Button
-                            style={{ marginLeft: "10px" }}
-                            variant="contained"
-                            color="error"
-                            startIcon={<LogoutIcon />}
-                            onClick={() => logout()}
-                        >
-                            Logout
-                        </Button>
-                    </Box>
-                </Toolbar>
-            </AppBar>
-            <Drawer
-                variant="permanent"
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    [`& .MuiDrawer-paper`]: {
-                        width: drawerWidth,
-                        boxSizing: "border-box",
-                    },
-                }}
-            >
-                <Toolbar />
-                <Box sx={{ overflow: "auto" }}>
-                    <List>
-                        <ListItem
-                            selected={contentName === "Demo"}
-                            button
-                            onClick={() =>
-                            {
-                                changeContent("Demo");
-                            }}
-                        >
-                            <ListItemText primary="Demo" />
-                        </ListItem>
-                        <Divider />
-                        <ListItem
-                            selected={contentName === "DefaultPage"}
-                            button
-                            onClick={() =>
-                            {
-                                changeContent("DefaultPage");
-                            }}
-                        >
-                            <ListItemText primary="DefaultPage" />
-                        </ListItem>
-                        <Divider />
-                    </List>
-                </Box>
-            </Drawer>
-            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                <Toolbar />
-                {contentName === "DefaultPage" && <DefaultPage />}
+              {!open ? <Menu /> : <Close />}
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Makarand Rajendra
+            </Typography>
+            <Box sx={{ flexGrow: 1 }} />
+            <Box sx={{ display: { xs: "flex" } }}>
+              <Typography variant="h6" noWrap component="div">
+                {currentUser?.email}
+              </Typography>
+              <Button
+                style={{ marginLeft: "10px" }}
+                variant="contained"
+                color="error"
+                startIcon={<LogoutIcon />}
+                onClick={() => logout()}
+              >
+                Logout
+              </Button>
             </Box>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant="temporary"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              width: drawerWidth,
+              boxSizing: "border-box",
+            },
+          }}
+          open={open}
+          onBackdropClick={() => setOpen(false)}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: "auto" }}>
+            <List>
+              <ListItem
+                selected={contentName === "DefaultPage"}
+                button
+                onClick={() => {
+                  changeContent("DefaultPage");
+                }}
+              >
+                <ListItemText primary="DefaultPage" />
+              </ListItem>
+              <Divider />
+            </List>
+          </Box>
+        </Drawer>
+        <Box
+          component="main"
+          sx={{ flexGrow: 1, p: 3 }}
+          className={clsx(classes.content, {
+            [classes.contentShift]: open,
+          })}
+        >
+          <Toolbar />
+          {contentName === "DefaultPage" && <DefaultPage />}
         </Box>
-    );
+      </Box>
+    </>
+  );
 }
--- src/Forms/SBI/Land or Site or Building/MainPage.jsx
+++ src/Forms/SBI/Land or Site or Building/MainPage.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
 
 import jsPDF from "jspdf";
+
+import { Divider, List, ListItem, ListItemText, Grid } from "@mui/material";
 
 //input forms
 import General from "../General";
@@ -36,7 +36,7 @@ import Undertaking from "../Undertaking";
 import ParticularValuerComment from "../ParticularValuerComment";
 import CodeOfConduct from "../CodeOfConduct";
 
-const initialValue = {
+let initialValue = {
   purposeOfValuation: "Banking/Financial institute",
   classificationOfArea1: "High",
   classificationOfArea2: "Urban",
@@ -50,118 +50,288 @@ const MainPage = ({ SelectedCaseDetails }) => {
   console.log(SelectedCaseDetails);
   const [data, setData] = useState(initialValue);
   const [PreviewOn, setPreviewOn] = useState(false);
+  const [contentName, setcontentName] = useState("General");
 
-  const getPDF = () => {
-    const doc = new jsPDF();
-    doc.autoTable({ html: "#General", theme: "grid" });
-    doc.autoTable({ html: "#my-table2", theme: "grid" });
-    doc.autoTable({ html: "#Part-A-B", theme: "grid" });
-    doc.autoTable({ html: "classification-of-construction-1", theme: "grid" });
-    doc.autoTable({ html: "classification-of-construction-2", theme: "grid" });
-    doc.autoTable({ html: "#details-of-valuation", theme: "grid" });
-    doc.autoTable({ html: "#total", theme: "grid" });
-    doc.autoTable({ html: "#Part-C", theme: "grid" });
-    doc.autoTable({ html: "#Part-D", theme: "grid" });
-    doc.autoTable({ html: "#Part-E", theme: "grid" });
-    doc.autoTable({ html: "#Part-F", theme: "grid" });
-    doc.autoTable({ html: "#Total-abstract", theme: "grid" });
+  const { DataByFVO, FVODetails, InitialCaseDetails } = SelectedCaseDetails;
+  console.log(SelectedCaseDetails);
 
-    // doc.autoTable({ html: "", theme: "grid" });
-    doc.save("table.pdf");
+  let date = InitialCaseDetails.dateOfInspection;
+  let doi =
+    date &&
+    new Date(date.seconds * 1000 + date.nanoseconds / 1000000)
+      .toISOString()
+      .slice(0, 10);
+  date = InitialCaseDetails.dateOfOutward;
+  let doo =
+    date &&
+    new Date(date.seconds * 1000 + date.nanoseconds / 1000000)
+      .toISOString()
+      .slice(0, 10);
+
+  initialValue = {
+    ...initialValue,
+    purposeOfValuation: InitialCaseDetails.purposeOfValuation,
+    dateOfInspection: doi,
+    dateOfValuation: doo,
+    completeAddress: InitialCaseDetails.address,
+    locality: InitialCaseDetails.locality,
+    district: InitialCaseDetails.district,
+    city: InitialCaseDetails.district,
+    latLang: InitialCaseDetails.latitude + "," + InitialCaseDetails.longitude,
+    north: DataByFVO.N,
+    south: DataByFVO.S,
+    west: DataByFVO.W,
+    east: DataByFVO.E,
   };
+
+  const changeContent = (value) => {
+    setcontentName(value);
+  };
+
+  // const getPDF = () => {
+  //   const doc = new jsPDF();
+  //   doc.autoTable({ html: "#General", theme: "grid" });
+  //   doc.autoTable({ html: "#my-table2", theme: "grid" });
+  //   doc.autoTable({ html: "#Part-A-B", theme: "grid" });
+  //   doc.autoTable({ html: "classification-of-construction-1", theme: "grid" });
+  //   doc.autoTable({ html: "classification-of-construction-2", theme: "grid" });
+  //   doc.autoTable({ html: "#details-of-valuation", theme: "grid" });
+  //   doc.autoTable({ html: "#total", theme: "grid" });
+  //   doc.autoTable({ html: "#Part-C", theme: "grid" });
+  //   doc.autoTable({ html: "#Part-D", theme: "grid" });
+  //   doc.autoTable({ html: "#Part-E", theme: "grid" });
+  //   doc.autoTable({ html: "#Part-F", theme: "grid" });
+  //   doc.autoTable({ html: "#Total-abstract", theme: "grid" });
+
+  //   // doc.autoTable({ html: "", theme: "grid" });
+  //   doc.save("table.pdf");
+  // };
 
   return (
     <div>
       {/* contains complete report of SBI - Land or Site or Building */}
-      <Box>
-        <General data={data} setData={setData} /> <br />
-        <CharacteristicOfSite data={data} setData={setData} />
-        <br />
-        <PartAB data={data} setData={setData} />
-        <br />
-        <SpecificationOfConstruction1 data={data} setData={setData} />
-        <br />
-        <SpecificationOfConstruction2 data={data} setData={setData} />
-        <br />
-        <DetailsOfValuation data={data} setData={setData} />
-        <br />
-        <PartC data={data} setData={setData} />
-        <br />
-        <PartD data={data} setData={setData} />
-        <br />
-        <PartE data={data} setData={setData} />
-        <br />
-        <PartF data={data} setData={setData} />
-        <br />
-        <TotalAbstractOfProperty data={data} setData={setData} />
-        <br />
-        <ImageSelector
-          data={data}
-          setData={setData}
-          SelectedCaseDetails={SelectedCaseDetails}
-        />
-        <Undertaking data={data} setData={setData} />
-        <br />
-        <ParticularValuerComment data={data} setData={setData} />
-        <br />
-        <CodeOfConduct data={data} setData={setData} />
-        <br />
-      </Box>
-      <Stack direction="row" spacing={2}>
-        <Button variant="contained" onClick={() => {}}>
-          Save
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => {
-            setPreviewOn(!PreviewOn);
-          }}
-        >
-          Preview
-        </Button>
-        {/* <Button variant="contained" onClick={getPDF}>
-          Get PDF
-        </Button> */}
-      </Stack>
-      {/* shows preview of SBI - Land or Site or Building */}
-      {PreviewOn && (
-        <Box>
-          <GeneralPreview data={data} />
-          <br />
-          <CharacteristicOfSitePreview data={data} />
-          <br />
-          <PartABPreview data={data} />
-          <br />
-          <SpecificationOfConstruction1Preview data={data} />
-          <br />
-          <SpecificationOfConstruction2Preview data={data} />
-          <br />
-          <DetailsOfValuationPreview data={data} />
-          <br />
-          <PartCPreview data={data} />
-          <br />
-          <PartDPreview data={data} />
-          <br />
-          <PartEPreview data={data} />
-          <br />
-          <PartFPreview data={data} />
-          <br />
-          <TotalAbstractOfPropertyPreview data={data} />
-          <br />
-          <Undertaking data={data} />
-          <br />
-          <ParticularValuerComment data={data} />
-          <br />
-          <CodeOfConduct data={data} />
-          <br />
-          <Undertaking data={data} />
-          <br />
-          <ParticularValuerComment data={data} />
-          <br />
-          <CodeOfConduct data={data} />
-          <br />
-        </Box>
-      )}
+      <Grid container spacing={2}>
+        <Grid item xs={2}>
+          <Box>
+            <List
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                maxWidth: "100%",
+              }}
+            >
+              <ListItem
+                selected={contentName === "General"}
+                button
+                onClick={() => {
+                  changeContent("General");
+                }}
+              >
+                <ListItemText primary="General" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Characteristic Of Site"}
+                button
+                onClick={() => {
+                  changeContent("Characteristic Of Site");
+                }}
+              >
+                <ListItemText primary="Characteristic Of Site" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Part AB"}
+                button
+                onClick={() => {
+                  changeContent("Part AB");
+                }}
+              >
+                <ListItemText primary="Part AB" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Specification Of Construction 1"}
+                button
+                onClick={() => {
+                  changeContent("Specification Of Construction 1");
+                }}
+              >
+                <ListItemText primary="Specification Of Construction 1" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Specification Of Construction 2"}
+                button
+                onClick={() => {
+                  changeContent("Specification Of Construction 2");
+                }}
+              >
+                <ListItemText primary="Specification Of Construction 2" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Details Of Valuation"}
+                button
+                onClick={() => {
+                  changeContent("Details Of Valuation");
+                }}
+              >
+                <ListItemText primary="Details Of Valuation" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Part C"}
+                button
+                onClick={() => {
+                  changeContent("Part C");
+                }}
+              >
+                <ListItemText primary="Part C" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Part D"}
+                button
+                onClick={() => {
+                  changeContent("Part D");
+                }}
+              >
+                <ListItemText primary="Part D" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Part E"}
+                button
+                onClick={() => {
+                  changeContent("Part E");
+                }}
+              >
+                <ListItemText primary="Part E" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Part F"}
+                button
+                onClick={() => {
+                  changeContent("Part F");
+                }}
+              >
+                <ListItemText primary="Part F" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Total Abstract Of Property"}
+                button
+                onClick={() => {
+                  changeContent("Total Abstract Of Property");
+                }}
+              >
+                <ListItemText primary="Total Abstract Of Property" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Image Selector"}
+                button
+                onClick={() => {
+                  changeContent("Image Selector");
+                }}
+              >
+                <ListItemText primary="Image Selector" />
+              </ListItem>
+              <Divider orientation="horizontal" flexItem />
+              <ListItem
+                selected={contentName === "Preview"}
+                button
+                onClick={() => {
+                  changeContent("Preview");
+                }}
+              >
+                <ListItemText primary="Preview" />
+              </ListItem>
+            </List>
+          </Box>
+        </Grid>
+        <Grid item xs={10}>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            {contentName === "General" && (
+              <General data={data} setData={setData} />
+            )}
+            {contentName === "Characteristic Of Site" && (
+              <CharacteristicOfSite data={data} setData={setData} />
+            )}
+            {contentName === "Part AB" && (
+              <PartAB data={data} setData={setData} />
+            )}
+            {contentName === "Specification Of Construction 1" && (
+              <SpecificationOfConstruction1 data={data} setData={setData} />
+            )}
+            {contentName === "Specification Of Construction 2" && (
+              <SpecificationOfConstruction2 data={data} setData={setData} />
+            )}
+            {contentName === "Details Of Valuation" && (
+              <DetailsOfValuation data={data} setData={setData} />
+            )}
+            {contentName === "Part C" && (
+              <PartC data={data} setData={setData} />
+            )}
+            {contentName === "Part D" && (
+              <PartD data={data} setData={setData} />
+            )}
+            {contentName === "Part E" && (
+              <PartE data={data} setData={setData} />
+            )}
+            {contentName === "Part F" && (
+              <PartF data={data} setData={setData} />
+            )}
+            {contentName === "Total Abstract Of Property" && (
+              <TotalAbstractOfProperty data={data} setData={setData} />
+            )}
+            {contentName === "Image Selector" && (
+              <ImageSelector data={data} setData={setData} />
+            )}
+            {contentName === "Preview" && (
+              <Box>
+                <GeneralPreview data={data} />
+                <br />
+                <CharacteristicOfSitePreview data={data} />
+                <br />
+                <PartABPreview data={data} />
+                <br />
+                <SpecificationOfConstruction1Preview data={data} />
+                <br />
+                <SpecificationOfConstruction2Preview data={data} />
+                <br />
+                <DetailsOfValuationPreview data={data} />
+                <br />
+                <PartCPreview data={data} />
+                <br />
+                <PartDPreview data={data} />
+                <br />
+                <PartEPreview data={data} />
+                <br />
+                <PartFPreview data={data} />
+                <br />
+                <TotalAbstractOfPropertyPreview data={data} />
+                <br />
+                <Undertaking data={data} />
+                <br />
+                <ParticularValuerComment data={data} />
+                <br />
+                <CodeOfConduct data={data} />
+                <br />
+                <Undertaking data={data} />
+                <br />
+                <ParticularValuerComment data={data} />
+                <br />
+                <CodeOfConduct data={data} />
+                <br />
+              </Box>
+            )}
+          </Box>
+        </Grid>
+      </Grid>
     </div>
   );
 };
